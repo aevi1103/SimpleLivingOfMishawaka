297eb8f81b029fad7b61962c9b5f2062e25a814e
aevi <aevirontos@gmail.com>
2019-08-18 10:24:40 -0400

    modified route

--- src/index.js
+++ src/index.js
@@ -41,25 +41,26 @@ ReactDOM.render(
     <Switch>
       <Switch>
 
-        <Route path="/index" render={props => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={props => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={props => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={props => <ProfilePage {...props} />}
-        />
-        <Route path="/login-page" render={props => <LoginPage {...props} />} />
+        <Route path="/index" render={props => <HomePage {...props} />} />
 
-        <Route exact path="/homepage" component={HomePage} />
+      {/* <Route
+        path="/nucleo-icons"
+        render={props => <NucleoIcons {...props} />}
+      />
+      <Route
+        path="/landing-page"
+        render={props => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={props => <ProfilePage {...props} />}
+      />
+      <Route path="/login-page" render={props => <LoginPage {...props} />} />
+
+      <Route exact path="/homepage" component={HomePage} /> */}
         
-        <Redirect to="/homepage" />
-        <Redirect from="/" to="/homepage" />
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
 
       </Switch>
     </Switch>
